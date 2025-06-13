ad4ed63293a058f4cbf71a26ac5de66d63928c47
mxbraun4 <maximilian.braun@posteo.de>
2025-06-13 14:17:19 +0200

    Add greeting intent handling

--- logic/constants.py
+++ logic/constants.py
@@ -8,10 +8,20 @@ disconfirm = ["No", "No, thank you", "No, thanks", "Nope", "Sorry, no", "No, I w
 
 thanks = ["Thanks!", "OK!", "I see", "Thanks a lot!", "Thank you.", "Alright, thank you!", "That's nice, thanks a lot :)", "Good, thanks!", "Thank you very much.", "Looks good, thank you!", "Great, thank you very much!", "Ok, thanks!", "Thank you for the answer."]
 bye = ["Goodbye!", "Bye-bye!", "Bye!", "Ok, bye then!", "That's all, bye!", "See you next time!", "Thanks for the chat, bye!"]
+greeting = [
+    "Hello", "Hi", "Hey", "Hi there", "Howdy", "Good morning",
+    "Good afternoon", "Good evening", "Greetings"
+]
 
 dialogue_flow_map = {
     "thanks": ["You are welcome!", "No problem.", "I'm glad I could help.", "Can I help you with something else?","Is there anything else I could do for you?"],
     "bye": ["Goodbye!", "Bye-bye!", "Have a nice day!", "See you next time!"],
+    "greeting": [
+        "Hello! How can I help you today?",
+        "Hi there! What can I do for you?",
+        "Greetings! How may I assist you?",
+        "Hey! How can I help?"
+    ],
     "sorry": ["Sorry! I couldn't understand that. Could you please try to rephrase?", "My apologies, I did not get what you mean.", "I'm sorry but could you rephrase the message, please?", "I'm not sure I can do this. Maybe you have another request for me?", "This is likely out of my expertise, can I help you with something else?", "This was a bit unclear. Could you rephrase it, please?", "Let's try another option. I'm afraid I don't have an answer for this."]
     }
 
